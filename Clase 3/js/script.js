1de6ec4217d4f6b846409b4f5b65ab7c328e3679
Ale <ale_6524@hotmail.com>
2023-06-29 09:58:07 -0300

    PRIMERA CLASE DE JS, CONCEPTOS, CONSOLA BROWSER

---
+++ Clase 3/js/script.js
@@ -0,0 +1,37 @@
+/* 
+Que es un lenguaje de programación?
+
+Para quien está hecho el lenguaje de programación?
+
+Javascript es un lenguaje de programación interpretado (compilado just in time), de alto nivel, 
+débilmente tipado 
+*/
+
+//tipos de datos: DATOS PRIMITIVOS
+//                Numbers---> 90, -90, 6.4, infinito, nan
+//                Strings---> Es una cadena de texto por ejemplo, "menganito es buen alumno" 
+//                Boolean---> Dos estados, true o false
+//                Undefined---> undefined, para datos que no estan definidos
+//                Null---> sin valor, ausencia de valor 
+
+
+/* console.log("hola"); */
+
+// Operadores Aritméticos---> Siempre devuelven un número
+
+//   +   
+//   -   *   /   %   
+
+// + 
+// Si se suma un número con un string se concatenan---> 4 + "hola"="4hola"
+
+
+//Comparadores---> Se necesitan a dos para funcionar, siempre devuelven Boolean
+//            ---> == igual
+//            ---> === estrictamente igual
+//            ---> != distinto 
+//            ---> !== estrictamente distinto 
+//            ---> < mayor
+//            ---> > menor
+//            ---> =< mayor o igual
+
